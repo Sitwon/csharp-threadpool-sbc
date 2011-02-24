f956f3c6b77915f81a4d4bab58b03de3fbc7c175
Ben Mendis <dragonwisard@gmail.com>
2011-02-24 10:44:51 -0500

    Making it multi-threaded

--- sample.cs
+++ sample.cs
@@ -9,10 +9,20 @@
 
 using System;
 using System.Collections;
+using System.Threading;
 using DfvDotNetCtl;
 
 public class MainApp
 {
+	private string _inputdoc;
+	private ManualResetEvent _doneEvent;
+
+	public MainApp(string inputdoc, ManualResetEvent doneEvent)
+	{
+		_inputdoc = inputdoc;
+		_doneEvent = doneEvent;
+	}
+
 	static public void Main(string[] args)
 	{
 		if (args.Length < 2)
@@ -20,14 +30,33 @@ public class MainApp
 			Console.WriteLine("usage: sample office-file out-file");
 			return;
 		}
-		
+		ManualResetEvent[] doneEvents = new ManualResetEvent[args.Length];
+		MainApp[] sbcJobs = new MainApp[args.Length];
+
+		Console.WriteLine("launching {0} tasks...", args.Length);
+		for (int i = 0; i < args.Length; i++)
+		{
+			doneEvents[i] = new ManualResetEvent(false);
+			MainApp job = new MainApp(args[i], doneEvents[i]);
+			sbcJobs[i] = job;
+			ThreadPool.QueueUserWorkItem(job.ThreadProc, i);
+		}
+		WaitHandle.WaitAll(doneEvents);
+		Console.WriteLine("All conversions are finished.");
+	}
+
+	public void ThreadProc(Object stateInfo)
+	{
+		Console.WriteLine("starting job {0}", (int)stateInfo);
 		DfvObj.Initialize();
 		DfvObj adfv = null;
+		string inputFilename = _inputdoc;
+		string outputFilename = inputFilename + ".pdf";
 		try
 		{
 			adfv = new DfvObj();
-			adfv.DocumentURI = args[0];
-			adfv.OutputFilePath = args[1];
+			adfv.DocumentURI = inputFilename;
+			adfv.OutputFilePath = outputFilename;
 //			adfv.PrinterName = "@SVG";
 			adfv.PrinterName = "@PDF";
 			adfv.ExitLevel = 4;
@@ -42,7 +71,7 @@ public class MainApp
 				ei.ErrorCode + "\n" + ei.ErrorMessage);
 			}
 			
-			Console.WriteLine("\nFormatting finished: '" + args[1] + "' created.");
+			Console.WriteLine("\nFormatting finished: '" + outputFilename + "' created.");
 		}
 		catch(DfvException e)
 		{
@@ -57,6 +86,8 @@ public class MainApp
 			if (adfv != null)
 				adfv.Dispose();
 			DfvObj.Terminate();
+			Console.WriteLine("finishing job {0}", (int)stateInfo);
+			_doneEvent.Set();
 		}
 	}
 }
